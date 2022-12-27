70b16cd7db7b38681f624891fb14bc8335f276dd
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2022-12-27 23:50:26 +0530

    js practice

---
+++ variable/variable.js
@@ -0,0 +1,12 @@
+let gender1 = "male";
+let age = 21;
+
+let gender2 = "female";
+let age2 = 18;
+
+if((gender1== "male")&&(age>=21)|| (gender2== female)&&(age2>=18)){
+  console.log("can get married")
+}
+else{
+  console.log("can not get married")
+}
