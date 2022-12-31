f888821cb9bdddd838b16e8333cdd035e14c31f5
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2022-12-31 23:57:12 +0530

    js practice

---
+++ nested/prime_no.js
@@ -0,0 +1,15 @@
+for(let i=1;i<=10;i++){
+  let number=i;
+  let count=0;
+  for(let j=1;j<=number;j++){
+    if(number%j==0){
+      count++;
+    }
+  }
+  if(count==2){
+    console.log(i,"prime number");
+    }
+  else{
+    console.log(i,"not a prime number");
+  }
+}
