b2972189df43a4bb8164fec94efc1c2a6e1669b7
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-01-14 07:54:56 +0530

    print non-prime number from 2 to given limit

---
+++ functions/print_not_prime_no.js
@@ -0,0 +1,20 @@
+// Use the same function to print Non-Primes from 2 to a given limit
+
+function printNonPrimes(limit) {
+    for (let i = 2; i <= limit; i++) {
+      let isPrime = true;
+  
+      for (let j = 2; j < i; j++) {
+        if (i % j === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+  
+      if (!isPrime) 
+      console.log(i);
+    }
+  }
+  
+  printNonPrimes(20);
+  
