61bd13d77a295dea68b3c3d1c4111674ddc81513
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-01-15 19:48:18 +0530

    prime or not

---
+++ functions/prime_or_not.js
@@ -0,0 +1,12 @@
+// Create a function to check if a number is Prime or Not
+
+function isPrime(num) {
+    if (num % 2 === 0) {
+        return false;
+      }
+    
+  
+    return true;
+  }
+  
+  console.log(isPrime(7));
